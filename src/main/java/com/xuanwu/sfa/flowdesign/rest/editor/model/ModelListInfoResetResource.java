748450244af166fa8536ef7060d5d010c4a42366
谭谈 <tantan@wxchina.com>
2017-09-07 17:38:37 +0800

    第一次提交

---
+++ src/main/java/com/xuanwu/sfa/flowdesign/rest/editor/model/ModelListInfoResetResource.java
@@ -0,0 +1,55 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xuanwu.sfa.flowdesign.rest.editor.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.ActivitiException;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Model;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Tijs Rademakers
+ */
+@RestController
+public class ModelListInfoResetResource implements ModelDataJsonConstants {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ModelListInfoResetResource.class);
+
+    @RequestMapping(value = "/modelList", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Map<String, Object> modelListInfo() {
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("key1", 11);
+        result.put("key2", 22);
+
+        return result;
+    }
+}
